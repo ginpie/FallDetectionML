c10bae4c70fd1c44be02314a7188b7ac08db0088
Jinpei Chen <jinpei.chen@anu.edu.au>
2019-09-10 21:16:25 +1000

    initial commit

--- venv/FallDetectionKNN.py
+++ venv/FallDetectionKNN.py
@@ -4,11 +4,20 @@ import scipy.optimize as opt
 import scipy.special
 import scipy.stats as stats
 import math
+import pandas as pd
+import operator
 
+# load data file in Numpy
 columns = ['counter', 'acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z']
 data = np.genfromtxt('test01.tsv')
 
 assert data.shape[1] == 8, 'you already ran this cell!'
 data = np.concatenate([data, np.ones((data.shape[0], 1))], axis=1)  # add a column of ones
 print(data.shape, "\n", data)
+
+
+# load data file in Pandas
+def loadData(file):
+    dataFrame = pd.read_csv(file)
+    return dataFrame
 
